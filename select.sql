722a333b1065b69908adf9eaf8e1b4499b652581
persiska1 <losdavid1@gmail.com>
2022-07-28 14:36:06 +0300

    Fixed SELECT

--- select.sql
+++ select.sql
@@ -1,89 +1,25 @@
--- 1 --
-SELECT
-    genre_id,
-    COUNT(artist_id) genre_count 
-FROM
-    artist_genre ag 
-GROUP BY
-	genre_id;
-
--- 2 --
-SELECT
-	c."name" , c.release_year
-FROM  collections AS c
+-- 1 -- 
+ select name, releasedate from album
+ where releasedate between  '2018-01-01' and '2018-12-31'
+ 
+-- 2 -- 
+SELECT track."name",track.tracklength
+FROM track
 WHERE
-	(c.release_year  >= '2019-01-01') AND (c.release_year  <= '2020-12-31');
+	tracklength =(SELECT MAX(track.tracklength) FROM track);
+-- 3 -- 
+select name, tracklength from track
+WHERE tracklength >= 3.5;
 
--- 3 --
-SELECT t."name", t.tracklength
-FROM track AS t 
-WHERE
-	t.tracklength >= (SELECT AVG(track.tracklength) FROM track);
+-- 4 --
+select name from collections
+where release_year between  '2018-01-01' and  '2020-01-01';
 
--- 4 -- 
-SELECT distinct artist."name"
+-- 5 -- 
+SELECT artist."name"
 FROM artist
-WHERE
-	artist."name" NOT IN (
-	SELECT DISTINCT artist."name"
-	FROM album as a
-	where a.releasedate <= '2020-01-01' AND a.releasedate >= '2020-12-31'
-	);
+WHERE artist."name" NOT LIKE '%% %%';
 
--- 5 --
-SELECT DISTINCT c."name" 
-FROM
-	collections as c
-left join collections_album_track as cat  on c.id = cat.collections_id
-left join track as t on t.id = cat.track_id
-left join album as a on a.id = t.album_id
-left join album_artist as aa on aa.album_id = a.id
-left join artist as a2 on aa.id = aa.artist_id
-where a.name like '%%Kirkorov%%'
-order by c.name;
-
--- 6 --
-SELECT a."name" 
-FROM album AS a
-left join album_artist aa on a.id = aa.album_id
-left join artist a2 on a2.id = aa.artist_id
-left join artist_genre ag  on a2.id = ag.artist_id
-left join genre as g on g.id = ag.genre_id
-group by a."name"
-having count(distinct g."name") > 1
-order by a."name";
-
--- 7 --
-SELECT t.name
-FROM track as t
-left join collections_album_track cat on t.id = cat.track_id
-WHER cat.track_id is null;
-
--- 8 -- 
-
-SELECT a2."name" , t.tracklength 
-FROM track as t
-left join album as a on a.id = t.album_id
-left join album_artist as aa on aa.album_id = a.id
-left join artist as a2 on a2.id = aa.artist_id
-GROP BY a2.name, t.tracklength 
-HAVING t.tracklength = (SELECT min(tracklength) FROM track)
-ORDER BY a2.name;
-
--- 9 -- 
-SELECT DISTINCT a.name
-FROM album as a
-left join track as t on t.album_id = a.id
-WHERE t.album_id in (
-    SELECT album_id
-    FROM track
-    GROUP BY album_id
- 	HAVING count(id) = (
-        SELECT count(id)
-        FROM track
-        GROUP BY album_id
-        ORDER BY count
-        LIMIT 1
-    )
-)
-ORDER BY a.name;
+-- 6 -- 
+select name from track
+where name like 'my%';
